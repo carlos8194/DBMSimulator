e45a38bcc0c5d70022a2e4ce40d0e68433051268
carlos.mellado <carlosmellado8194@hotmail.com>
2017-02-06 19:46:56 -0600

    6/2/17

--- src/connection/ConnectionStatistics.java
+++ src/connection/ConnectionStatistics.java
@@ -4,14 +4,14 @@ package connection;
  * Created by Carlos on 03/02/2017.
  */
 public class ConnectionStatistics {
-    public double entryTimeModule1; // when we finally got attended
-    public double TimeModule1; // arrival time
-    public double entryTimeModule2;
-    public double TimeModule2;
-    public double entryTimeModule3;
-    public double TimeModule13;
-    public double entryTimeModule4;
-    public double TimeModule4;
-    public double entryTimeModule5;
-    public double TimeModule5;
+    double entryTimeModule1; // when we finally got attended
+    double TimeModule1; // arrival time
+    double entryTimeModule2;
+    double TimeModule2;
+    double entryTimeModule3;
+    double TimeModule13;
+    double entryTimeModule4;
+    double TimeModule4;
+    double entryTimeModule5;
+    double TimeModule5;
 }
